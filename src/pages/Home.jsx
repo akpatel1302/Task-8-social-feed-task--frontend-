5cca4e6bf4cf76eb69cd827723d9bd5999cc45d5
Ayush Patel <ayushk0208@gmail.com>
2024-04-29 19:23:24 +0530

    fix: Get api and display data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -187,6 +187,253 @@
 
 // *********************************get api called***************
 
+// import React, { useState, useEffect } from "react";
+// import { useFetchPostsQuery, useCreatePostsMutation } from "../api/SignupApi";
+// import CreatePostModal from "../component/CreatePostModal";
+// import Navbar from "../component/Navbar";
+// import {
+//   Grid,
+//   Card,
+//   CardContent,
+//   Typography,
+//   makeStyles,
+//   Button,
+// } from "@material-ui/core";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     padding: theme.spacing(3),
+//   },
+//   card: {
+//     height: "100%",
+//     display: "flex",
+//     flexDirection: "column",
+//   },
+//   cardContent: {
+//     flexGrow: 1,
+//   },
+//   createPostButton: {
+//     marginBottom: theme.spacing(2),
+//   },
+// }));
+
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) === " ") {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) === 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
+
+// function authenticateToken(accessToken) {
+//   return accessToken !== "";
+// }
+
+// const Home = () => {
+//   const classes = useStyles();
+//   const [showModal, setShowModal] = useState(false);
+//   const { data, isLoading, isError, error } = useFetchPostsQuery({
+//     page: 1,
+//     perPage: 20,
+//     search: "",
+//     isMyPostsOnly: false,
+//     isPrivate: false,
+//     accessToken: getCookie("accessToken"),
+//   });
+//   const [
+//     createPost,
+//     { isLoading: isCreatingPost, isError: isCreateError, error: createError },
+//   ] = useCreatePostsMutation();
+
+//   const handleCreatePost = async (postData) => {
+//     try {
+//       postData.isPrivate = false;
+//       const accessToken = getCookie("accessToken");
+//       if (authenticateToken(accessToken)) {
+//         await createPost({ postData, accessToken });
+//         setShowModal(false);
+//       } else {
+//         console.error("Access token authentication failed");
+//       }
+//     } catch (error) {
+//       console.error("Error creating post:", error);
+//     }
+//   };
+
+//   return (
+//     <div className={classes.root}>
+//       <Navbar />
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         onClick={() => setShowModal(true)}
+//         className={classes.createPostButton}
+//       >
+//         Create Post
+//       </Button>
+//       {showModal && (
+//         <CreatePostModal
+//           onClose={() => setShowModal(false)}
+//           onSubmit={handleCreatePost}
+//         />
+//       )}
+//       <Grid container spacing={3}>
+//         {data?.posts?.map((post) => (
+//           <Grid item xs={12} sm={6} md={4} key={post.id}>
+//             <Card className={classes.card}>
+//               <CardContent className={classes.cardContent}>
+//                 <Typography gutterBottom variant="h5" component="h2">
+//                   {post.title}
+//                 </Typography>
+//                 <Typography>{post.content}</Typography>
+//               </CardContent>
+//             </Card>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+// **************************************************new
+
+// import React, { useState, useEffect } from "react";
+// import { useFetchPostsQuery, useCreatePostsMutation } from "../api/SignupApi";
+// import CreatePostModal from "../component/CreatePostModal";
+// import Navbar from "../component/Navbar";
+// import {
+//   Grid,
+//   Card,
+//   CardContent,
+//   Typography,
+//   makeStyles,
+//   Button,
+// } from "@material-ui/core";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     padding: theme.spacing(3),
+//   },
+//   card: {
+//     height: "100%",
+//     display: "flex",
+//     flexDirection: "column",
+//   },
+//   cardContent: {
+//     flexGrow: 1,
+//   },
+//   createPostButton: {
+//     marginBottom: theme.spacing(2),
+//   },
+// }));
+
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) === " ") {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) === 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
+
+// function authenticateToken(accessToken) {
+//   return accessToken !== "";
+// }
+
+// const Home = () => {
+//   const classes = useStyles();
+//   const [showModal, setShowModal] = useState(false);
+//   const { data, isLoading, isError, error } = useFetchPostsQuery({
+//     page: 1,
+//     perPage: 20,
+//     search: "",
+//     isMyPostsOnly: false,
+//     isPrivate: false,
+//     accessToken: getCookie("accessToken"),
+//   });
+//   const [
+//     createPost,
+//     { isLoading: isCreatingPost, isError: isCreateError, error: createError },
+//   ] = useCreatePostsMutation();
+
+//   const handleCreatePost = async (postData) => {
+//     try {
+//       postData.isPrivate = false;
+//       const accessToken = getCookie("accessToken");
+//       if (authenticateToken(accessToken)) {
+//         await createPost({ postData, accessToken });
+//         setShowModal(false);
+//       } else {
+//         console.error("Access token authentication failed");
+//       }
+//     } catch (error) {
+//       console.error("Error creating post:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (!isLoading && !isError && data) {
+//       console.log("Posts data:", data);
+//       console.log(data.data.data);
+//     }
+//   }, [data, isLoading, isError]);
+
+//   return (
+//     <div className={classes.root}>
+//       <Navbar />
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         onClick={() => setShowModal(true)}
+//         className={classes.createPostButton}
+//       >
+//         Create Post
+//       </Button>
+//       {showModal && (
+//         <CreatePostModal
+//           onClose={() => setShowModal(false)}
+//           onSubmit={handleCreatePost}
+//         />
+//       )}
+//       <Grid container spacing={3}>
+//         {data?.posts?.map((post) => (
+//           <Grid item xs={12} sm={6} md={4} key={post.id}>
+//             <Card className={classes.card}>
+//               <CardContent className={classes.cardContent}>
+//                 <Typography gutterBottom variant="h5" component="h2">
+//                   {post.title}
+//                 </Typography>
+//                 <Typography>{post.content}</Typography>
+//               </CardContent>
+//             </Card>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+// ************************allnew
+
 import React, { useState, useEffect } from "react";
 import { useFetchPostsQuery, useCreatePostsMutation } from "../api/SignupApi";
 import CreatePostModal from "../component/CreatePostModal";
@@ -268,6 +515,12 @@ const Home = () => {
     }
   };
 
+  useEffect(() => {
+    if (!isLoading && !isError && data) {
+      console.log("Posts data:", data.data.data);
+    }
+  }, [data, isLoading, isError]);
+
   return (
     <div className={classes.root}>
       <Navbar />
@@ -286,8 +539,8 @@ const Home = () => {
         />
       )}
       <Grid container spacing={3}>
-        {data?.posts?.map((post) => (
-          <Grid item xs={12} sm={6} md={4} key={post.id}>
+        {data?.data?.data?.map((post) => (
+          <Grid item xs={12} sm={6} md={4} key={post._id}>
             <Card className={classes.card}>
               <CardContent className={classes.cardContent}>
                 <Typography gutterBottom variant="h5" component="h2">
